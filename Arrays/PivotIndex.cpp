8a35bcc8ec6d6fd150e4512525e55d94893d5d00
karan2808 <karantank@Karans-MacBook-Pro.local>
2021-02-03 23:39:54 -0500

    updated

--- Arrays/PivotIndex.cpp
+++ Arrays/PivotIndex.cpp
@@ -1,21 +1,26 @@
-# include <iostream>
-# include <vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-class Solution{
-    public:
-    int findPivot(vector<int> nums) {
+class Solution
+{
+public:
+    int findPivot(vector<int> nums)
+    {
         // find the total sum of nums
         int total = 0;
-        for (int x: nums) {
+        for (int x : nums)
+        {
             total += x;
         }
 
         // keep a track of left sum
         int leftsum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (leftsum == total - leftsum - nums[i]) {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (leftsum == total - leftsum - nums[i])
+            {
                 return i;
             }
             leftsum += nums[i];
@@ -34,7 +39,8 @@ void printVec(vector<int> values)
     cout << endl;
 }
 
-int main() {
+int main()
+{
     Solution mySol;
     vector<int> nums = {1, 2, -1, 4, 5, 8, -2, 4, -2, -2};
     cout << "For the array ";
