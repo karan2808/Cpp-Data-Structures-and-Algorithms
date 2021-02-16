34f74822807781d4512833a11ba5e2fd4717daaa
karan2808 <karantank@Karans-MacBook-Pro.local>
2021-02-15 22:13:35 -0500

    templates in cpp

---
+++ Misc/Templates.cpp
@@ -0,0 +1,6 @@
+/**
+ * The idea behind templates is to pass data type as a prameter so that we dont need to write the same code for 
+ * different data types. For example, the function sort() may be needed for different data types. Rather than writing
+ * different codes, we can write one sort function and pass the data type as a parameter. 
+ * 
+ */
